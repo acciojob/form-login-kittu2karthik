0fa9092caeffb91455927b6fdd20573c6382f0b1
kittu2karthik <71444538+kittu2karthik@users.noreply.github.com>
2025-11-03 00:10:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-function getFormvalue() {
+function getFormvalue(e) {
     //Write your code here
+	e.preventDefault();
+	const firstName = document.querySelector('input[name="fname"]').value;
+	const lastName = document.querySelector('input[name="lname"]').value;
+	alert(`${firstName.trim()} ${lastName.trim()}`)
 
 }
